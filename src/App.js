780b21c7e430dd1ac917d3a8a53d338d6c3660ec
phichidung <benevolentwarriors@gmail.com>
2019-10-02 14:22:54 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>This is react-context-api-test</h1>
+            </div>
+        );
+    }
+}
